97975e550edcf59ddd26c1899a4a523cd961f2a8
mfalguier <maxime.falguier@ux-republic.com>
2017-07-26 23:52:02 +0200

    redirect home

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-// in server.js
 const express = require('express');
 const app = express();
 
-// Since the root/src dir contains our index.html
+// Since the root/ dir contains our index.html
 app.use(express.static(__dirname + '/'));
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  // home redirect
+  if (req.accepts('html')) {
+    res.render('404', res.redirect("/"));
+    return;
+  }
+
+});
 
 // Heroku bydefault set an ENV variable called PORT=443
 //  so that you can access your site with https default port.
 // Falback port will be 8080; basically for pre-production test in localhost
 // You will use $ npm run prod for this
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8090);
